6229d66d77fc175d2680306b27f5c7b57f6d27e6
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2021-02-21 17:14:22 +0200

    Added orders

---
+++ Training.AspNetCore/Training.AspNetCore/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Training.AspNetCore.Database;
+using Training.AspNetCore.Database.Models;
+
+namespace Training.AspNetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ContactRequestDbContext _context;
+
+        public OrderController(ContactRequestDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Order> SendOrder(Order order)
+        {
+            order.TimeReceived = DateTime.Now;
+
+            _context.Add(order);
+            _context.SaveChanges();
+
+            return Ok("Order received");
+        }
+    }
+}
---
+++ Training.AspNetCore/Training.AspNetCore/Database/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Training.AspNetCore.Database.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string BillingAddress { get; set; }
+        public bool Book1 { get; set; }
+        public bool Book2 { get; set; }
+        public bool Book3 { get; set; }
+        public DateTime TimeReceived { get; set; }
+    }
+}
